0583c5cf127585d14eeaea1ab7a9af123197f245
TuncayArican <tuncayarican07@gmail.com>
2022-02-01 23:40:53 +0300

    ReverseOrder has been made with JS

---
+++ JAVA SCRIPT/Homework_Projects/ReverseWord/app.js
@@ -0,0 +1,35 @@
+// With a classical method
+/* function reverseOrder(word){
+        let newWord=""
+        for (let i= word.length-1; i>=0; i--) {
+            newWord = newWord + word[i]
+    }
+    return newWord
+}
+console.log(reverseOrder("Merhaba nasılsın"));  */
+
+
+// with methods
+/* function reverseWord(word){
+    word2=word.split("").reverse().join("")
+    return word2
+}
+console.log(reverseWord("Merhaba nasılsın")); */
+
+/* Studying */
+
+/* let word3="Tuncay is very hardworking student"
+let word4=word3.split(" ")
+word4.reverse()
+word4.join("")
+console.log(word4); */
+/* 
+let word6=["ali", "mehmet", "merhaba"]
+/* word6.reverse()
+console.log(word6); */
+/* word6=word6.join(" ")
+console.log(word6) */
+
+/* let word8="Tuncay is very hardworking student"
+word8=word8.split(" ")
+console.log(word8); */
