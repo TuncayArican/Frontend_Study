b91f096e33de3de66ea7768fec1d4c0568abbd0d
TuncayArican <tuncayarican07@gmail.com>
2022-02-03 00:27:12 +0300

    Reversed word function has been written

--- JAVA SCRIPT/Homework_Projects/ReverseWord/app.js
+++ JAVA SCRIPT/Homework_Projects/ReverseWord/app.js
@@ -1,35 +1,22 @@
-// With a classical method
-/* function reverseOrder(word){
-        let newWord=""
-        for (let i= word.length-1; i>=0; i--) {
-            newWord = newWord + word[i]
-    }
-    return newWord
-}
-console.log(reverseOrder("Merhaba nasılsın"));  */
+/* - Write a function that takes an array of characters and reverses the letters in place.
 
+- Examples:
 
-// with methods
+  - reverse("Clarusway Rocks!") ➞ !skcoR yawsuralC
+
+  - reverse("Happy") ➞ yppaH */
+
 /* function reverseWord(word){
-    word2=word.split("").reverse().join("")
-    return word2
+      let word2=word.split("").reverse().join("") 
+      return word2
 }
-console.log(reverseWord("Merhaba nasılsın")); */
+console.log(reverseWord("Clarusway Rocks!")); */
 
-/* Studying */
+function reverseWord(word){
+    let word2=""
+    for (let i= word.length-1; i>=0 ; i--) {
+        word2+=word[i]
+    } return word2
+}
+console.log(reverseWord("Clarusway Rocks!"));
 
-/* let word3="Tuncay is very hardworking student"
-let word4=word3.split(" ")
-word4.reverse()
-word4.join("")
-console.log(word4); */
-/* 
-let word6=["ali", "mehmet", "merhaba"]
-/* word6.reverse()
-console.log(word6); */
-/* word6=word6.join(" ")
-console.log(word6) */
-
-/* let word8="Tuncay is very hardworking student"
-word8=word8.split(" ")
-console.log(word8); */
---
+++ JAVA SCRIPT/Homework_Projects/ReverseWord/readme.md
@@ -0,0 +1,7 @@
+- Write a function that takes an array of characters and reverses the letters in place.
+
+- Examples:
+
+  - reverse("Clarusway Rocks!") ➞ !skcoR yawsuralC
+
+  - reverse("Happy") ➞ yppaH
