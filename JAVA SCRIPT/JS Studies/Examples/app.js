51f40354dc1e4b36b73f242686a13475c75cab68
TuncayArican <tuncayarican07@gmail.com>
2022-02-04 02:01:35 +0300

    JS examples have been made

--- JAVA SCRIPT/JS Studies/Examples/app.js
+++ JAVA SCRIPT/JS Studies/Examples/app.js
@@ -20,6 +20,9 @@ console.log(addWord(word)); */
 
 
 
+
+
+
 //How Much is True?
 //countTrue([true, false, false, true, false]) ➞ 2
 
@@ -41,16 +44,7 @@ function countTrue(list2){
 
 
 
-//In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided dice. If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.
 
-// Can you reach your friend's tile number in the next roll? Create a function that takes your position a and your friend's position b and returns a boolean representation of whether it's possible to earn a bonus on any dice roll.
-
-// Examples
-// possibleBonus(3, 7) ➞ true
-
-// possibleBonus(1, 9) ➞ false
-
-// possibleBonus(5, 3) ➞ fals
 
 
 
@@ -79,7 +73,7 @@ Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome. */
 
 
-let word1=prompt("Please enter a number")
+/* let word1=prompt("Please enter a number")
 let word2=""
 function polidrome(word1){
     for (i=word1.length-1; i>=0 ; i--){
@@ -90,3 +84,152 @@ function polidrome(word1){
         return (`${word1} is not a palindrome`);
     }
 }console.log(polidrome(word1));
+ */
+
+
+
+
+
+
+/* Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+Examples
+minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+minMax([2334454, 5]) ➞ [5, 2334454]
+
+minMax([1]) ➞ [1, 1] */
+
+
+/* let list1=[8, 9, 3, 1, 11, 11]
+list1.sort(function(a, b){return a - b});
+console.log(list1); */
+
+/* Crea-1te a function that returns true if the first array can be nested inside the second.
+
+arr1 can be nested inside arr2 if:
+
+arr1's min is greater than arr2's min.
+arr1's max is less than arr2's max.
+Examples
+canNest([1, 2, 3, 4], [0, 6]) ➞ true
+
+canNest([3, 1], [4, 0]) ➞ true
+
+canNest([9, 9, 8], [8, 9]) ➞ false
+
+canNest([1, 2, 3, 4], [2, 3]) ➞ false
+ */
+
+let list1=[3, 1];
+let list2=[4,0];
+function canNest(list1,list2){
+let list3=list1.sort(function(a, b){return a - b})
+let list4=list2.sort(function(a, b){return a - b})
+if ((list4[0]<list3[0]) && (list4[list4.length-1] > list3[list3.length-1])) {
+    return true; 
+}else{
+   return false;
+}
+}console.log(canNest(list1,list2)); 
+
+
+/* The left shift operation is similar to multiplication by powers of two.
+
+Sample calculation using the left shift operator (<<):
+
+10 << 3 = 10 * 2^3 = 10 * 8 = 80
+-32 << 2 = -32 * 2^2 = -32 * 4 = -128
+5 << 2 = 5 * 2^2 = 5 * 4 = 20
+Write a function that mimics (without the use of <<) the left shift operator and returns the result from the two given integers.
+
+Examples
+shiftToLeft(5, 2) ➞ 20
+
+shiftToLeft(10, 3) ➞ 80
+
+shiftToLeft(-32, 2) ➞ -128
+
+shiftToLeft(-6, 5) ➞ -192
+
+shiftToLeft(12, 4) ➞ 192
+
+shiftToLeft(46, 6) ➞ 2944 */
+
+
+
+/* const REGEXP = /blue|red/
+
+"red flag".match(REGEXP)  // red
+"blue flag".match(REGEXP)  // blue
+
+// Matches "blue" in "blue flag" and "red" in "red flag".
+Create a regular expression to match all red flag and blue flag in a string. You must use | in your expression. Flags can come in any order.
+
+Examples
+"red flag blue flag".match(REGEXP) ➞ ["red flag", "blue flag"]
+
+"yellow flag red flag blue flag green flag".match(REGEXP) ➞ ["red flag", "blue flag"]
+
+"pink flag red flag black flag blue flag green flag red flag ".match(REGEXP) ➞ ["red flag", "blue flag", "red flag"]
+Notes
+Check the Resources tab if you get stuck. */
+
+
+
+
+/* Given a total due and an array representing the amount of change in your pocket, determine whether or not you are able to pay for the item. Change will always be represented in the following order: quarters, dimes, nickels, pennies.
+
+To illustrate: changeEnough([25, 20, 5, 0], 4.25) should yield true, since having 25 quarters, 20 dimes, 5 nickels and 0 pennies gives you 6.25 + 2 + .25 + 0 = 8.50.
+
+Examples
+changeEnough([2, 100, 0, 0], 14.11) ➞ false
+
+changeEnough([0, 0, 20, 5], 0.75) ➞ true
+
+changeEnough([30, 40, 20, 5], 12.55) ➞ true
+
+changeEnough([10, 0, 0, 50], 3.85) ➞ false
+
+changeEnough([1, 0, 5, 219], 19.99) ➞ false
+Notes
+quarter: 25 cents / $0.25
+dime: 10 cents / $0.10
+nickel: 5 cents / $0.05
+penny: 1 cent / $0.01 */
+
+
+
+/* Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
+
+Examples
+sevenBoom([1, 2, 3, 4, 5, 6, 7]) ➞ "Boom!"
+// 7 contains the number seven.
+
+sevenBoom([8, 6, 33, 100]) ➞ "there is no 7 in the array"
+// None of the items contain 7 within them.
+
+sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
+// 97 contains the number seven.
+
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring. */
+
+
+
+
+
+/* Given a string s, find the length of the longest substring without repeating characters. */
