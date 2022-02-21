96216fe47e1a80f9ea674cd0f72a4d8dfb9e32bd
TuncayArican <tuncayarican07@gmail.com>
2022-02-21 22:39:21 +0300

    Added

--- JAVA SCRIPT/Sessions/s11/index.js
+++ JAVA SCRIPT/Sessions/s11/index.js
@@ -44,21 +44,106 @@ for (const [key, value] of Object.entries(person)) {
 
 // * Methods
 
-const person = {
-  firstName: 'Barry',
-  lastName: 'Mitchell',
-  birthYear: 1977,
-  skills: ['JS', 'AWS', 'Docker', 'Python'],
-  employed: true,
-  calcAgeBad: function (bYear) {
-    return 2022 - bYear;
-  },
+// const person = {
+//   firstName: 'Barry',
+//   lastName: 'Mitchell',
+//   birthYear: 1977,
+//   skills: ['JS', 'AWS', 'Docker', 'Python'],
+//   employed: true,
+//   calcAgeBad: function (bYear) {
+//     return 2022 - bYear;
+//   },
+//   calcAge() {
+//     /* this.age = 2022 - this.birthYear;
+//     return this.age; */
+//     return 2022 - this.birthYear;
+//   },
+// };
+
+// console.log(person.calcAge());
+// // console.log(person.age);
+
+// function f1() {
+//   console.log(this);
+// }
+
+// f1();
+
+// ! THIS examples
+/* 
+const calcAge = function (birthYear) {
+  console.log('Regular Function in Global Scope');
+  console.log(this);
+  console.log(2022 - birthYear);
+};
+
+calcAge(1977);
+
+const calcAgeArrow = (birthYear) => {
+  console.log('Arrow Function in Global Scope');
+  console.log(this);
+  console.log(2022 - birthYear);
+};
+
+calcAgeArrow(1980); */
+
+const teacher = {
+  birthYear: 1985,
   calcAge() {
-    /* this.age = 2022 - this.birthYear;
-    return this.age; */
-    return 2022 - this.birthYear;
+    console.log('Regular Function in Object');
+    console.log(this);
+    console.log(2022 - this.birthYear);
+  },
+  calcAgeArrow: () => {
+    console.log('Arrow Function in Object');
+    console.log(this);
+    console.log(2022 - this.birthYear);
+  },
+  calcAgeDelayed() {
+    setTimeout(function () {
+      console.log('Regular Function in setTimeout');
+      console.log(this);
+      this.calcAge();
+    }, 2000);
+  },
+  calcAgeDelayedArrow: function () {
+    setTimeout(() => {
+      console.log('Arrow Function in setTimeout');
+      console.log(this);
+      this.calcAge();
+    }, 2000);
   },
 };
 
-console.log(person.calcAge());
-console.log(person.age);
+const student = {
+  birthYear: 1995,
+};
+
+student.calcAge = teacher.calcAge;
+
+student.calcAge();
+
+const myFunc = teacher.calcAge;
+
+teacher.calcAge();
+myFunc();
+
+// teacher.calcAge();
+// teacher.calcAgeArrow();
+// teacher.calcAgeDelayed();
+// teacher.calcAgeDelayedArrow();
+
+let anObject = {
+  firstName: 'John',
+  age: 30,
+  isMarried: false,
+  introduce: () => {
+    return `${this.firstName} is ${this.age} years old.`;
+  },
+  introduce2: function () {
+    return `${this.firstName} is ${this.age} years old.`;
+  },
+};
+
+console.log(anObject.introduce());
+console.log(anObject.introduce2());
