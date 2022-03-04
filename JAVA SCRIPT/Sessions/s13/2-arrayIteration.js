cf5c4d10aa441b5271452fc2ee5bbf0f25d98d71
TuncayArican <tuncayarican07@gmail.com>
2022-03-04 21:09:13 +0300

    New session has been added

--- JAVA SCRIPT/Sessions/s13/2-arrayIteration.js
+++ JAVA SCRIPT/Sessions/s13/2-arrayIteration.js
@@ -17,7 +17,6 @@ students.forEach((student) => console.log(student));
 
 //? Example
 const payments = [100, 300, -200, 500, -400, 225, 1000, -700];
-
 let sum = 0;
 payments.forEach((p) => (sum += p));
 console.log("SUM:", sum);
@@ -40,6 +39,34 @@ payments.forEach((p, i) => {
 //* multiply each element of array by 2
 //*-------------------------------------------------------
 const numbers = [2, 3, 5, 6, 8];
-
 const doubled = numbers.map((n) => n * 2);
 console.log(doubled, numbers);
+
+//* if a grade is less than 50, increase it by 20% oherwise
+//* increase it by 10% and store all value in the grades array.
+//*-------------------------------------------------------
+let grades = [30, 50, 77, 38, 44, 80];
+
+grades = grades.map((grade) =>
+  (grade > 50 ? grade * 1.1 : grade * 1.2).toFixed(2)
+);
+console.log(grades);
+
+// grades = grades.map((grade) => {
+//  return grade > 50 ? grade * 1.1 : grade * 1.2;
+// });
+
+// console.log(grades);
+
+//? Modified
+const gradesInfo = grades.map((grade, index) => {
+  const newGrade = (grade > 50 ? grade * 1.1 : grade * 1.2).toFixed(2);
+  return `${index + 1}. Student's Grade: ${newGrade}`;
+});
+console.log(gradesInfo);
+
+
+//* Test the each item of the words array whether palindromic or not.
+//* Return the result as an array which consists of true or
+//* false values for each item
+//*-------------------------------------------------------
