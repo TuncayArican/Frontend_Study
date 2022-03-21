f7096c2f45532bb73c1358152308753e14ea8ad0
TuncayArican <tuncayarican07@gmail.com>
2022-03-22 00:37:04 +0300

    Last session added

--- React/react-styling/src/App.js
+++ React/react-styling/src/App.js
@@ -1,5 +1,6 @@
 import Card from "./components/card/Card";
 import Inline from "./components/inline-style/Inline";
+import ModuleBtn from "./components/moduleCss/ModuleBtn";
 import StyleSheet from "./components/regular-style/StyleSheet";
 import data from "./data";
 
@@ -9,6 +10,8 @@ const App = () => {
       <Inline/>
       <StyleSheet secenek={false}/>
       <Card veri={data}/>
+      <ModuleBtn btnStyle="birinci" btnName="Kaydet"/>
+      <ModuleBtn btnStyle="ikinci" btnName="Sil"/>
     
    
     </>
--- React/react-styling/src/components/moduleCss/ModuleBtn.jsx
+++ React/react-styling/src/components/moduleCss/ModuleBtn.jsx
@@ -1,1 +1,16 @@
+import styles from "./moduleBtn.module.css"
 
+import React from 'react'
+
+const ModuleBtn = ({btnStyle, btnName}) => {
+  return (
+    <div className= {styles.main}>
+        <button className={styles[btnStyle]}>
+        {btnName}
+        </button>
+
+    </div>
+  )
+}
+
+export default ModuleBtn
