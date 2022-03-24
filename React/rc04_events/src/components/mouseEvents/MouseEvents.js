ded89eb94f87d52ed0f309c5681725e7a6f2edb3
TuncayArican <tuncayarican07@gmail.com>
2022-03-24 21:49:47 +0300

    New react session (Events) added

---
+++ React/rc04_events/src/components/mouseEvents/MouseEvents.js
@@ -0,0 +1,47 @@
+import './styles.css';
+
+const MouseEvents = () => {
+  // click event
+  const handleClick = (e) => {
+    // console.log(e);
+    // console.log(e.type);
+    // console.log(e.target);
+    console.log(e.currentTarget);
+    console.log(e.target.tagName);
+    e.target.tagName === "SPAN" && e.currentTarget.remove();
+  }
+  // doubleClick event
+  // mouseMove event
+  return (
+    <>
+      <h2>MouseEvents</h2>
+      <p>
+        <span>X</span> and Y
+      </p>
+      <p>
+        <span></span> and
+      </p>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <ul>
+          <li id="todo-1" onClick={handleClick}>
+            todo item 1 <span>X</span>
+          </li>
+          <li id="todo-2 ">
+            todo item 2 <span>X</span>
+          </li>
+          <li>
+            todo item 3 <span>X</span>
+          </li>
+          <li>
+            todo item 4 <span>X</span>
+          </li>
+          <li>
+            todo item 5 <span>X</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default MouseEvents;
